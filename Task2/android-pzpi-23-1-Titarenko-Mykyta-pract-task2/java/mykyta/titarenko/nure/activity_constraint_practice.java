cd1550016be0b3f0c529ab64b3bdcfe4a67b669d
Mykyta Titarenko <mykyta.titarenko@nure.ua>
2024-10-06 10:20:46 +0300

    Add files via upload

---
+++ Task2/android-pzpi-23-1-Titarenko-Mykyta-pract-task2/java/mykyta/titarenko/nure/activity_constraint_practice.java
@@ -0,0 +1,30 @@
+package mykyta.titarenko.nure;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.concurrent.TimeUnit;
+
+public class activity_constraint_practice extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_constraint_practice);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
